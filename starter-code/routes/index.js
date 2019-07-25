39bf4ee36b0e71c476bb3e00a53f6ab7b7729eb7
pl0erre <luca.angona@web.de>
2019-07-25 11:44:24 +0200

    getting data from coindesk is working

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  
+  axios.get('https://api.coindesk.com/v1/bpi/historical/close.json?start=2013-09-01&end=2013-09-05')
+  .then(function (response) {
+    console.log(response.data); // working fine
+    res.render('index');
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
 });
+
+
+
+
 
 module.exports = router;
